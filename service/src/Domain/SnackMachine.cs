8a397a34f158be677053d97f7b2be1cf725abac6
Edvaldo Junior <ejunnior@gmail.com>
2022-11-03 20:14:53 +0100

    Initial Commit (#1)
    Co-authored-by: Edvaldo Junior <EJUNIOR@volvocars.com>

---
+++ service/src/Domain/SnackMachine.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain;
+
+using Common;
+
+public class SnackMachine : Entity
+{
+    public Money MoneyInside { get; private set; }
+    public Money MoneyInTransaction { get; private set; }
+
+    public void BuySnack()
+    {
+        MoneyInside += MoneyInTransaction;
+
+        //MoneyInTransaction = 0;
+    }
+
+    public void InsertMoney(Money money)
+    {
+        MoneyInTransaction += money;
+    }
+
+    public void ReturnMoney()
+    {
+        //MoneyInTransaction = 0;
+    }
+}
